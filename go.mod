778c59f980ce415ed8e705c8061b294394e0e7af
Andrew Skurlatov <a.skurlatov@gmail.com>
2019-04-18 01:24:32 +0300

    Websocket: client initialization and Market Depth method

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/andskur/hbdm-go
+
+go 1.12
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gofrs/uuid v3.2.0+incompatible
+	github.com/gorilla/websocket v1.4.0
+)
---
+++ websocket.go
@@ -0,0 +1,305 @@
+package hbdm
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"sync"
+
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/websocket"
+)
+
+var mu sync.Mutex
+
+// HBDM websocket API URL's
+const (
+	wsMarketData = "wss://www.hbdm.com/ws"
+	wsOrders     = "wss://api.hbdm.com/notification"
+)
+
+// responseChannels handles all incoming data from the hbdm connection.
+type responseChannels struct {
+	MarketDepth map[string]chan WsDepthMarketResponse
+
+	ErrorFeed chan error
+}
+
+// WSClient represents a JSON RPC v2 Connection over Websocket,
+type WSClient struct {
+	conn    *websocket.Conn
+	Updates *responseChannels
+	done    chan struct{}
+}
+
+// NewWSClient creates a new hbm Websocket API client
+func NewWSClient() (*WSClient, error) {
+	conn, _, err := websocket.DefaultDialer.Dial(wsMarketData, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	handler := responseChannels{
+		MarketDepth: make(map[string]chan WsDepthMarketResponse),
+
+		ErrorFeed: make(chan error),
+	}
+
+	client := &WSClient{
+		conn:    conn,
+		Updates: &handler,
+
+		done: make(chan struct{}),
+	}
+
+	go client.handle()
+
+	return client, nil
+}
+
+// wsHbdmRequest is top-level hbdm request to Websocket API
+type wsHbdmRequest struct {
+	Sub string `json:"sub"`
+	Id  string `json:"id"`
+}
+
+// wsHbdmResponse is top-level response from hbdm Websocket API
+type wsHbdmResponse struct {
+	Ch string `json:"ch"`
+	Ts int    `json:"ts"`
+}
+
+//
+func (c *WSClient) handle() error {
+	defer close(c.done)
+	go func() {
+		<-c.done
+		c.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		msg, err := gzipCompress(message)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		ok, err := c.checkPing(msg)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if ok {
+			continue
+		}
+
+		method, symbol, err := c.parseMethod(msg)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		switch method {
+		case "depth":
+			var resp WsDepthMarketResponse
+			if err := json.Unmarshal(msg, &resp); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			mu.Lock()
+			c.Updates.MarketDepth[symbol] <- resp
+			mu.Unlock()
+		default:
+			continue
+		}
+
+	}
+}
+
+// Ping represent "Ping" request from Websocket server
+type Ping struct {
+	Ping int `json:"Ping"`
+}
+
+// Pong represent "Pong" request to Websocket server
+type Pong struct {
+	Pong int `json:"Pong"`
+}
+
+// checkPing check if message is "ping" if yes - send "pong"
+func (c *WSClient) checkPing(msg []byte) (bool, error) {
+	var res map[string]interface{}
+
+	if err := json.Unmarshal(msg, &res); err != nil {
+		return false, err
+	}
+
+	for k := range res {
+		if strings.Contains(k, `ping`) {
+			goto Pong
+		}
+		return false, nil
+	}
+
+Pong:
+	var ping Ping
+
+	err := json.Unmarshal(msg, &ping)
+	if err != nil {
+		return true, err
+	}
+
+	pong := Pong{Pong: ping.Ping}
+
+	jsonPong, err := json.Marshal(pong)
+	if err != nil {
+		return true, err
+	}
+
+	err = c.conn.WriteMessage(websocket.TextMessage, jsonPong)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
+
+// parseMethod parse API method from Websocket response message
+func (c WSClient) parseMethod(msg []byte) (method, symbol string, err error) {
+	var resp wsHbdmResponse
+
+	if err := json.Unmarshal(msg, &resp); err != nil {
+		log.Printf("%s", err)
+		return "", "", err
+	}
+
+	slice := strings.Split(resp.Ch, ".")
+	length := len(slice)
+
+	if length > 1 {
+		symbol = slice[1]
+	}
+
+	if length > 2 {
+		method = slice[2]
+	}
+
+	return
+}
+
+// Close closes the Websocket connected to the hbdm api.
+func (c *WSClient) Close() {
+	// Cleanly close the connection by sending a close message and then
+	// waiting (with timeout) for the server to close the connection.
+	err := c.conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Println("write close:", err)
+		return
+	}
+	c.conn.Close()
+
+	for _, channel := range c.Updates.MarketDepth {
+		close(channel)
+	}
+
+	close(c.Updates.ErrorFeed)
+
+	c.Updates.MarketDepth = make(map[string]chan WsDepthMarketResponse)
+	c.Updates.ErrorFeed = make(chan error)
+
+}
+
+// WsDepthMarketResponse is Market Depth method top-level response
+type WsDepthMarketResponse struct {
+	Ch   string          `json:"ch"`
+	Ts   int             `json:"ts"`
+	Tick MarketDepthTick `json:"tick"`
+}
+
+// MarketDepthTick is Depth Offer main data
+type MarketDepthTick struct {
+	Ch      string  `json:"ch"`
+	Mrid    int     `json:"mrid"`
+	Id      int     `json:"id"`
+	Ts      int     `json:"ts"`
+	Version int     `json:"version"`
+	Bids    []Offer `json:"bids"`
+	Asks    []Offer `json:"asks"`
+}
+
+// Offer is Offer with Contract Price and Amount
+type Offer struct {
+	Price  float64 `json:"price"`
+	Amount float64 `json:"amount"`
+}
+
+// UnmarshalJSON make correct Json Unmarshaling fro Offer structure
+func (o *Offer) UnmarshalJSON(b []byte) error {
+	var offer []float64
+
+	if err := json.Unmarshal(b, &offer); err != nil {
+		return fmt.Errorf("unmarshalling: %v", err)
+	}
+
+	o.Price = offer[0]
+	o.Amount = offer[1]
+	return nil
+}
+
+// SubscribeMarketDepth subscribe to websocket Market Depth data
+func (c *WSClient) SubscribeMarketDepth(symbol string) (<-chan WsDepthMarketResponse, error) {
+	if c.conn == nil {
+		return nil, errors.New("connection is unitialized")
+	}
+
+	sub := fmt.Sprintf("market.%s.depth.step0", symbol)
+
+	id, err := uuid.NewV4()
+	if err != nil {
+		return nil, err
+	}
+
+	var request = wsHbdmRequest{Sub: sub, Id: id.String()}
+
+	msg, err := json.Marshal(request)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if err := c.conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		log.Println("write", err)
+	}
+
+	mu.Lock()
+	if c.Updates.MarketDepth[symbol] == nil {
+		c.Updates.MarketDepth[symbol] = make(chan WsDepthMarketResponse)
+	}
+	mu.Unlock()
+
+	return c.Updates.MarketDepth[symbol], nil
+}
+
+// gzipCompress compress Gzip response
+func gzipCompress(in []byte) ([]byte, error) {
+	reader, err := gzip.NewReader(bytes.NewReader(in))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+	msg, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
